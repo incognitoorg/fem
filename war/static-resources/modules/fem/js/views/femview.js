dd2df51c86124a7f97c2a3758dd418fa3057289c
vishwanatharondekar <vishwanatharondekar@gmail.com>
2013-03-15 22:18:07 +0530

    Interaction for menu, reponsive.

--- war/static-resources/modules/fem/js/views/femview.js
+++ war/static-resources/modules/fem/js/views/femview.js
@@ -1,16 +1,33 @@
 define(function(require){
 	var Backbone = require('backbone');
-	
 	var Foundation = require('css!libraries/foundation/css/foundation');
-	var Foundation = require('libraries/foundation/js/foundation/foundation');
+	//var Foundation = require('libraries/foundation/js/foundation/foundation');
 	
 	var femView = Backbone.View.extend({
 		initialize : function(options){
 			this.render();
+			
+			this.eventShowView('js-dashboard');
 		},
 		template : Handlebars.compile(require('text!../../templates/femtemplate.html')),
 		render : function(){
 			$(this.el).html(this.template())
+		},
+		events : {
+			'click .js-menu' : 'eventShowView',
+			'click .js-back-to-menu' : 'eventShowMenu'
+		},
+		eventShowView : function(event){
+			var clickedMenu = (event.currentTarget && $(event.currentTarget).data('menu')) ||event
+			//var clickedMenu = $(event.currentTarget).data('menu');
+			this.$('.js-view-item').hide();
+			this.$('.' + clickedMenu).show();
+			this.$('.left-side-menu').addClass('hide-for-small');
+			this.$('.right-panel').removeClass('hide-for-small');
+		},
+		eventShowMenu : function(){
+			this.$('.left-side-menu').removeClass('hide-for-small');
+			this.$('.right-panel').addClass('hide-for-small');
 		}
 	});
 	return femView;
