40abdb20256c00bae93f5327202e910a336ba5ae
vishwanatharondekar <vishwanatharondekar@gmail.com>
2013-03-15 22:44:21 +0530

    Removed foundation JS.

--- war/static-resources/modules/fem/js/views/femview.js
+++ war/static-resources/modules/fem/js/views/femview.js
@@ -1,7 +1,6 @@
 define(function(require){
 	var Backbone = require('backbone');
 	var Foundation = require('css!libraries/foundation/css/foundation');
-	//var Foundation = require('libraries/foundation/js/foundation/foundation');
 	
 	var femView = Backbone.View.extend({
 		initialize : function(options){
