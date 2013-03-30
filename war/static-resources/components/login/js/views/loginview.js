d9c18f83f99bdf0f3de7ef812c12979ec309ca49
vishwanatharondekar <vishwanatharondekar@gmail.com>
2013-03-30 20:50:33 +0530

    Added actual login method. Also using Sandbox instead of Facade.

--- war/static-resources/components/login/js/views/loginview.js
+++ war/static-resources/components/login/js/views/loginview.js
@@ -3,7 +3,7 @@ define(function(require){
 	var Normalize = require('css!libraries/foundation/css/normalize.css');
 	var Foundation = require('css!libraries/foundation/css/foundation');
 	
-	var Facade = require('facade');
+	var Sandbox = require('sandbox');
 	var CSS = require('css!./../../css/login.css');
 	
 	var FBAPI = require('components/fbapi/fbapi');
@@ -26,16 +26,33 @@ define(function(require){
 			
 		},
 		eventDoFacebookLogin : function(){
-			FBAPI.checkAndDoLogin({callback : this.loginSucceded, context : this});
+			FBAPI.checkAndDoLogin({callback : this.doActualLogin, context : this});
 		},
 		eventDoGoogleLogin : function(){
-			GoogleAPI.checkAndDoLogin({callback : this.loginSucceded, context : this});
+			GoogleAPI.checkAndDoLogin({callback : this.doActualLogin, context : this});
+		},
+		doActualLogin : function(data){
+			var ajaxOptions = {
+				url : '_ah/api/userendpoint/v1/user/doLogin',
+				callback : this.loginSucceded, 
+				errorCallback : this.somethingBadHappend,
+				context : this,
+				dataType: 'json',
+				contentType: 'application/json',
+				type : 'POST',
+				data : JSON.stringify(data)
+				
+			};
+			Sandbox.doAjax(ajaxOptions);
 		},
 		loginSucceded : function(response){
 			console.log('In loginSucceded');
 			console.log(response);
 			this.hide();
-			Facade.publish('LOGIN:SUCCESS', {data : this.normalizeUserData(response)});
+			Sandbox.publish('LOGIN:SUCCESS', {data : this.normalizeUserData(response)});
+		},
+		somethingBadHappend : function(){
+			console.log('Something bad happened, find out who did that and kill them');
 		},
 		hide : function(){
 			$(this.el).hide();
