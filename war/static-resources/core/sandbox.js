bb9c288a4255f01fc2d411d31a3db1439a3815b7
vishwanatharondekar <vishwanatharondekar@gmail.com>
2013-03-30 16:12:31 +0530

    Replacing facade with sandbox.

--- war/static-resources/core/sandbox.js
+++ war/static-resources/core/sandbox.js
@@ -1,114 +1,108 @@
 "use strict";
-define(['mediator'], function (mediator) {
+define(['mediator', 'http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js', 'locallayer'], function (mediator, modernizer) {
 
+	
+	var isSyncEnabled = false;
+	
 	var Sandbox = {};
-
-	// * **param:** {string} subscriber Module name
-	// * **param:** {string} channel Event name
-	// * **param:** {object} callback Module
 	Sandbox.subscribe = function (channel, /*subscriber,*/ callback, context) {
 		return mediator.subscribe(channel, /*subscriber,*/ callback, context || this);
 	};
 
-	// * **param:** {string} channel Event name
 	Sandbox.publish = function (channel) {
 		mediator.publish.apply(mediator, arguments);
 	};
 
-	
 	Sandbox.registerChildren = function(children, parent){
 		mediator.registerChildren.apply(mediator, arguments);
 	};
-	
 	
 	Sandbox.destroy = function(component){
 		mediator.destroy.apply(mediator, arguments);
 	};
 	
-	Sandbox.decorate = function(baseObject, decorations){
+	Sandbox.doAjax = function(options){
+		var url = options.url;
+		var type = options.type;
+		var contentType = options.contentType;
+		var dataType = options.dataType;
+		var callback = options.callback;
+		var errorCallback = options.errorCallback;
+		var context = options.context;
 		
-		if(debugMode){
-
-			var overridden = [];
-
-			for ( var index in baseObject) {
-				if(decorations[index]){
-					overridden.push(index);
-				}
-			}
-
-			if(overridden.length>0){
-				//console.error('Protocol breach, using decorate for overriding');
-				console.warn('Protocol breach, using decorate for overriding, You should be arrested for this.');
-				console.log('Overridden properties');
-				console.log(overridden.join());
-			}
-		}
-		
-		if(baseObject.prototype instanceof Backbone.View){
-			if(decorations.extendedEvents){
-				_.extend(baseObject.prototype.events, decorations.extendedEvents);
-			}
-			return baseObject.extend(decorations);
-		}
-		
-		if(baseObject.getInstance){
-			return _.extend(baseObject.getInstance(), decorations);
-		}
-		
-		if(decorations.extendedRules){
-			_.extend(baseObject.rules, decorations.extendedRules);
-		}
-		return _.extend(baseObject, decorations);
-	};
-	
-	Sandbox.override = function(baseObject,overriddens){
-		
-		if(debugMode){
-			
-			var baseObjectForSize = baseObject;
-			
-			if(baseObject.prototype instanceof Backbone.View){
-				baseObjectForSize = baseObject.prototype;
-			} else if(baseObjectForSize.getInstance) {
-				baseObjectForSize = baseObjectForSize.getInstance();
-			}
-			
-			
-			var baseObjectSize = _.size(baseObjectForSize);
-			var overriddensize = _.size(overriddens);
-			var overrideRatio = overriddensize/baseObjectSize;
-			
-			if(overrideRatio>=0.5){
-				console.warn('You are overriding just too much, consider excluding base file and copy all properties/functions in extension');
-				console.log('Properties/functions to copy');
-				console.log(_.difference(_.keys(baseObjectForSize), _.keys(overriddens)));
-			}
-			
-		}
-		
-		
-		
-		if(baseObject.prototype instanceof Backbone.View){
-			if(overriddens.extendedEvents){
-				_.extend(baseObject.prototype.events, overriddens.extendedEvents);
-			}
-			return baseObject.extend(overriddens);
-		}
-		
-		if(baseObject.getInstance){
-			return _.extend(baseObject.getInstance(), overriddens);
-		}
-		
-		if(overriddens.extendedRules){
-			_.extend(baseObject.rules, overriddens.extendedRules);
-		}
-		
-		return _.extend(baseObject, overriddens);
+		$.ajax({
+		  'url':url,
+		  'type': type, 
+		  'contentType':contentType,
+		  'dataType': dataType,
+		  'success': function(response){
+			  
+			  if(response.data){
+				  var data = response.data;
+				 if(response.status==="success"){
+					 callback.call(context, data);
+				 }else if(response.status==="fail"){
+					 errorCallback.call(context, data);
+				 }else if(response.status==="error"){
+					 errorCallback.call(context, response.message);
+				 } 
+			  }else{
+				  callback.call(context, response);
+			  }
+			  
+		  },
+		  'error': function(response){
+			  errorCallback.call(context, response);
+			  
+		  }
+		});
 	};
 	
 	
+	Sandbox.View = Backbone.View;
+	Sandbox.Model = Backbone.Model;
+	Sandbox.Collection = Backbone.Collection;
+	Sandbox.Router = Backbone.Router;
+	Sandbox.History = Backbone.History;
+	
 
+	Sandbox.doUpdate= function(data){
+		if(Modernizr.localstorage && isSyncEnabled){
+			loallayer.doUpdate(data);
+		} else {
+			this.doAjax(data);
+		}
+	};
+	
+	Sandbox.doGet = function(data){
+		if(Modernizr.localstorage && isSyncEnabled){
+			loallayer.doUpdate(data);
+		} else {
+			this.doAjax(data);
+		}
+	};
+	
+	Sandbox.doDelete = function(data){
+		if(Modernizr.localstorage && isSyncEnabled){
+			loallayer.doDelete(data);
+		} else {
+			this.doAjax(data);
+		}
+	};
+	
+	Sandbox.doAdd = function(data){
+		if(Modernizr.localstorage && isSyncEnabled){
+			loallayer.doAdd(data);
+		} else {
+			this.doAjax(data);
+		}
+	};
+	
+	Sandbox.doSync = function(){
+		
+	};
+	
+	
 	return Sandbox;
 
 });
