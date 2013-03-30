52ad4554e27145afd822cfd96cfff530c6ccaeff
vishwanatharondekar <vishwanatharondekar@gmail.com>
2013-03-30 23:11:03 +0530

    commiting locallayer.js

---
+++ war/static-resources/core/locallayer.js
@@ -0,0 +1,20 @@
+define(function(require) {
+
+	return {
+		doUpdate : function(){
+			
+		},
+		doAdd : function(){
+			
+		},
+		doDelete : function(){
+			
+		}, 
+		doGet : function(){
+			
+		},
+		doSync : function(){
+			
+		}
+	};
+});
