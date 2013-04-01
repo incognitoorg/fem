8bc43abab55e8d235c3aebb4cae78640c2fe9ab3
vishwanatharondekar <vishwanatharondekar@gmail.com>
2013-04-02 00:17:08 +0530

    GroupEndPoint

--- src/com/fem/google/cloud/endpoints/Group.java
+++ src/com/fem/google/cloud/endpoints/Group.java
@@ -12,18 +12,18 @@ public class Group {
 	
 	@PrimaryKey
 	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
-	private String groupId;
+	private Long groupId;
 
 	private String groupName;
 	private String groupType;
-	private String groupOwerId;
+	private String groupOwnerId;
 	
 	
 	
-	public String getGroupId() {
+	public Long getGroupId() {
 		return groupId;
 	}
-	public void setGroupId(String groupId) {
+	public void setGroupId(Long groupId) {
 		this.groupId = groupId;
 	}
 	public String getGroupName() {
@@ -39,10 +39,10 @@ public class Group {
 		this.groupType = groupType;
 	}
 	public String getGroupOwerId() {
-		return groupOwerId;
+		return groupOwnerId;
 	}
-	public void setGroupOwerId(String groupOwerId) {
-		this.groupOwerId = groupOwerId;
+	public void setGroupOwerId(String groupOwnerId) {
+		this.groupOwnerId = groupOwnerId;
 	}
 
 	
