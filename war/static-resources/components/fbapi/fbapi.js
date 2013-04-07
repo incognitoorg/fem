bfd3104595dc427f619e9e18f4482d89de603266
vishwanatharondekar <vishwanatharondekar@gmail.com>
2013-04-07 15:27:14 +0530

    FbApi for local.

--- war/static-resources/components/fbapi/fbapi.js
+++ war/static-resources/components/fbapi/fbapi.js
@@ -4,8 +4,8 @@ define(function(require) {
 	
 	 // init the Facebook JS SDK
     FB.init( {
-    	//appId: '476625022390528', // http://localhost:8888		//srikanth's local appId
-        appId: '505558516167469', // http://fem2-vishwanath.appspot.com
+    	appId: '476625022390528', // http://localhost:8888		//srikanth's local appId
+        //appId: '505558516167469', // http://fem2-vishwanath.appspot.com
         //channelUrl: 'http://localhost:8888/channel.html', // Channel File for x-domain communication
         status: true, // check the login status upon init?
         cookie: true, // set sessions cookies to allow your server to access the session?
