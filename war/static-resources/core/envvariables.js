32f09f8859c362bf071f9f674736c0042528751f
vishwanatharondekar <vishwanatharondekar@gmail.com>
2013-04-14 00:22:48 +0530

    Double quotes, ahhh, Not that I mean double quotes as in double quotes. There were double quotes inside single quotes. So can I say we had triple quotes.

--- war/static-resources/core/envvariables.js
+++ war/static-resources/core/envvariables.js
@@ -6,14 +6,14 @@ define(function(require) {
 		FB_APP_ID : '476625022390528',
 		GOOGLE_CLIENT_ID : '675356629669.apps.googleusercontent.com',
 		GOOGLE_API_KEY : 'AIzaSyCxvFWYp8uk3RxCSEaVEo_FLYeqQVUelpg',
-		GOOGLE_API_SCOPE : '"https://www.googleapis.com/auth/plus.me https://www.google.com/m8/feeds"',
+		GOOGLE_API_SCOPE : 'https://www.googleapis.com/auth/plus.me https://www.google.com/m8/feeds',
 		
 		//https://fem-dev.appspot.com/
 		/*API_URL : "https://fem-dev.appspot.com/",
 		FB_APP_ID : '605170889512500', 
 		GOOGLE_CLIENT_ID : '675356629669.apps.googleusercontent.com',
 		GOOGLE_API_KEY : 'AIzaSyCxvFWYp8uk3RxCSEaVEo_FLYeqQVUelpg',
-		GOOGLE_API_SCOPE : '"https://www.googleapis.com/auth/plus.me https://www.google.com/m8/feeds"',*/
+		GOOGLE_API_SCOPE : 'https://www.googleapis.com/auth/plus.me https://www.google.com/m8/feeds',*/
 		
 		DEBUG_MODE : ''
 	};
