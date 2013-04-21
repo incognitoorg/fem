e5f2af6700852e26c9274e6b96e14d02da1acb9f
vishwanatharondekar <vishwanatharondekar@gmail.com>
2013-04-21 20:48:53 +0530

    Build.js for initial setup.

--- r-js-optimizer/tools/build.js
+++ r-js-optimizer/tools/build.js
@@ -1,7 +1,7 @@
 {
 	baseUrl: "./../../war/static-resources",
 	include: '../page-js/fem.js',
-	optimize: "uglify",
+	optimize: "uglify2",
 	uglify: {
         toplevel: true,
         ascii_only: true,
@@ -30,10 +30,20 @@
         //with the default options, no need to specify
         //any of these properties.
         output: {
-            beautify: true
+            beautify: false
         },
         compress: {
-            sequences: false
+            sequences: true,
+            join_vars: true,
+            if_return: true,
+            properties    : true,  // optimize property access a["Foo"] to a.foo
+            conditionals  : true,  // optimize if-s and conditional expressions
+            comparisons   : true,  // optimize comparisons
+            evaluate      : true,  // evaluate constant expressions
+            booleans      : true,  // optimize boolean expressions
+            loops         : true,  // optimize loops
+            drop_debugger : true,  // discard “debugger” statements
+            dead_code     : true,  // discard unreachable code
         },
         warnings: true,
         mangle: false
@@ -57,7 +67,6 @@
 		css : 'libraries/require/css',
 		normalize : 'libraries/require/normalize',
 		async : 'libraries/require/async',
-		/*fb : 'libraries/require/fb',*/
 		text : 'libraries/require/text',
 		facade : 'core/facade',
 		envvariables : 'core/envvariables',
@@ -84,7 +93,7 @@
 	 //Inlines the text for any text! dependencies, to avoid the separate
     //async XMLHttpRequest calls to load those dependencies.
     inlineText: true,
-    /*fileExclusionRegExp: /\.css/,*/
+    /*fileExclusionRegExp: //.css/,*/
 	/*include: '../page-js/home.js',*/
     //appDir: '../www',
     /*mainConfigFile: '../www/js/common.js',*/
@@ -130,60 +139,41 @@
         //needs to be delayed from loading until common.js finishes.
         //That loading sequence is controlled in page1.js.
         
-       /* {
-        	//module names are relative to baseUrl/paths config
-            name: 'app/main1',
-            exclude: ['../common']
-        	name :'bootloaders/bootloader/bootloader',
-            include: ['bootloaders/bootloader/bootloader'],
-            exclude: ['backbone','handlebars','modules/common/ibepagecontroller/ibepagecontroller']
-        },
-        {
-        	name :'components/decidepagehandler/decidepagehandler',
-            include: ['components/decidepagehandler/decidepagehandler'],
-            exclude: ['backbone','handlebars','modules/common/ibepagecontroller/ibepagecontroller']
-        },
-        {
-        	name :'modules/common/ibehomepage/ibehomepage',
-            include: ['modules/common/ibehomepage/ibehomepage'],
-            exclude: ['backbone']
-        },
-        {
-		    name :'modules/air/airdecidepagehandler/airdecidepagehandler',
-		    include : ['modules/air/airdecidepagehandler/airdecidepagehandler'],
-		    exclude :['backbone','handlebars','underscore','jquery', 'modules/common/ibehomepage/ibehomepage','components/decidepagehandler/decidepagehandler'] 
-        },
-        {
-        	name :'modules/common/ibepagecontroller/ibepagecontroller',
-            include: ['modules/common/ibepagecontroller/ibepagecontroller'],
-            exclude: ['backbone','handlebars','underscore','jquery', 'modules/common/ibehomepage/ibehomepage']
-        },
-        {
-        	name :'modules/air/bookingreview/bookingreview',
-            include: ['modules/air/bookingreview/bookingreview'],
-        	exclude: ['backbone','handlebars','underscore','jquery','libraries/jquery-ui/jquery-ui']
-        },
-        {
-        	name :'modules/air/aircart/airCart',
-            include: ['modules/air/aircart/airCart'],
-        	exclude: ['backbone','handlebars','underscore','jquery']
-        },
-        {
-        	name :'modules/common/shippinginfo/shippinginfo',
-            include: ['modules/common/shippinginfo/shippinginfo'],
-        	exclude: ['backbone','handlebars','underscore','jquery']
-        },
-        {
-        	name :'modules/air/bookingsummary/bookingsummary',
-            include: ['modules/air/bookingsummary/bookingsummary'],
-        	exclude: ['backbone','handlebars','underscore','jquery']
-        }*/
         {
         	name :'bootloaders/fembootloader/fembootloader',
             include: ['bootloaders/fembootloader/fembootloader'],
-        	exclude: ['backbone','handlebars','underscore','jquery']
+        	exclude: []
+        },
+        {
+        	name :'components/login/login',
+            include: ['components/login/login'],
+        	exclude: ['backbone','handlebars','underscore','jquery', 'bootloaders/fembootloader/fembootloader']
+        },
+        {
+        	name :'modules/fem/fem',
+            include: ['modules/fem/fem'],
+        	exclude: ['backbone','handlebars','underscore','jquery', 'bootloaders/fembootloader/fembootloader','components/login/login']
+        },
+        {
+        	name :'modules/addgroup/addgroup',
+            include: ['modules/addgroup/addgroup'],
+        	exclude: ['backbone','handlebars','underscore','jquery', 'bootloaders/fembootloader/fembootloader','components/login/login', 'modules/fem/fem']
+        },
+        {
+        	name :'modules/selectgroup/selectgroup',
+            include: ['modules/selectgroup/selectgroup'],
+        	exclude: ['backbone','handlebars','underscore','jquery', 'bootloaders/fembootloader/fembootloader','components/login/login', 'modules/fem/fem','modules/addgroup/addgroup']
         }
         
 
+        
+/*        var componentPathMapper = {
+        		'js-create-group'		:		'modules/addgroup/addgroup',
+        		'js-edit-group'			:		'modules/selectgroup/selectgroup',
+        		'js-new-expense'		:		'',
+        		'js-expense-history'	:		'',
+        		'js-dashboard'			:		'',
+        		'js-profile'			:		''
+        	};*/
     ]
 }
