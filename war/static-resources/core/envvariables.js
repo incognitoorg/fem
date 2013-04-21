9674c56623b1171dfc2633e4364b0a6067d0cf99
vishwanatharondekar <vishwanatharondekar@gmail.com>
2013-04-21 20:55:58 +0530

    added flag for settings.

--- war/static-resources/core/envvariables.js
+++ war/static-resources/core/envvariables.js
@@ -1,20 +1,24 @@
 define(function(require) {
-	return {
-
-		//localhost:8888
-		API_URL : "",
-		FB_APP_ID : '476625022390528',
-		GOOGLE_CLIENT_ID : '675356629669.apps.googleusercontent.com',
-		GOOGLE_API_KEY : 'AIzaSyCxvFWYp8uk3RxCSEaVEo_FLYeqQVUelpg',
-		GOOGLE_API_SCOPE : 'https://www.googleapis.com/auth/plus.me https://www.google.com/m8/feeds',
-		
-		//https://fem-dev.appspot.com/
-		/*API_URL : "https://fem-dev.appspot.com/",
-		FB_APP_ID : '605170889512500', 
-		GOOGLE_CLIENT_ID : '675356629669.apps.googleusercontent.com',
-		GOOGLE_API_KEY : 'AIzaSyCxvFWYp8uk3RxCSEaVEo_FLYeqQVUelpg',
-		GOOGLE_API_SCOPE : 'https://www.googleapis.com/auth/plus.me https://www.google.com/m8/feeds',*/
-		
-		DEBUG_MODE : ''
-	};
+	
+	var mode='local';
+	
+	if(mode==='dev'){
+		return {
+			API_URL : "https://fem-dev.appspot.com/",
+			FB_APP_ID : '605170889512500', 
+			GOOGLE_CLIENT_ID : '675356629669.apps.googleusercontent.com',
+			GOOGLE_API_KEY : 'AIzaSyCxvFWYp8uk3RxCSEaVEo_FLYeqQVUelpg',
+			GOOGLE_API_SCOPE : 'https://www.googleapis.com/auth/plus.me https://www.google.com/m8/feeds'
+			
+		};
+	} else if(mode==='local'){
+		return {
+			API_URL : "",
+			FB_APP_ID : '476625022390528',
+			GOOGLE_CLIENT_ID : '675356629669.apps.googleusercontent.com',
+			GOOGLE_API_KEY : 'AIzaSyCxvFWYp8uk3RxCSEaVEo_FLYeqQVUelpg',
+			GOOGLE_API_SCOPE : 'https://www.googleapis.com/auth/plus.me https://www.google.com/m8/feeds',
+			
+		};
+	}
 });
