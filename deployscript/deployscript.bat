a34c577fb31d2268222f076044e08d978f099efe
vishwanatharondekar <vishwanatharondekar@gmail.com>
2013-04-22 23:35:11 +0530

    deployscript ready.

--- deployscript/deployscript.bat
+++ deployscript/deployscript.bat
@@ -2,18 +2,27 @@ echo "substituting dev settings"
 "C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/mode='local'/mode='dev'/g ..\war\static-resources\core\envvariables.js
 "C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/\/static-resources\//\/built-static-resources\//g ../war/boilerplate.js
 "C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/'fem.appcache'/'builtfem.appcache'/g ../war/index.html
+"C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/"static-resources\//"built-static-resources\//g ../war/index.html
+"C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/1.0/1.0.1/g ../war/builtfem.appcache
+
+
+
+
+
 
 cd ..\r-js-optimizer\
-REM optimize.cmd
+REM call optimize.cmd
 cd ..\deployscript\
 
-
-"C:\Users\vishwanath\Downloads\appengine-java-sdk-1.7.5\appengine-java-sdk-1.7.5\bin\appcfg.cmd" update ../war
+call "C:\Users\vishwanath\Downloads\appengine-java-sdk-1.7.5\appengine-java-sdk-1.7.5\bin\appcfg.cmd" update ../war
 
 
 echo "Coming back to local"
 "C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/mode='dev'/mode='local'/g ..\war\static-resources\core\envvariables.js
 "C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/built-static-resources/static-resources/g ../war/boilerplate.js
 "C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/'builtfem.appcache'/'fem.appcache'/g ../war/index.html
+"C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/"built-static-resources\//"static-resources\//g ../war/index.html
+"C:\Program Files (x86)\GnuWin32\bin\sed" -ci s/1.0.1.1.1.1.1/1.0/g ../war/builtfem.appcache
+
 echo "Done"
 
