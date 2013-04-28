bb5bc9b3b4d251ac89b2de0d49ef39b69539598d
vishwanatharondekar <vishwanatharondekar@gmail.com>
2013-04-28 18:33:05 +0530

    Updated to new files.

--- r-js-optimizer/tools/build.js
+++ r-js-optimizer/tools/build.js
@@ -59,7 +59,27 @@
     },
     skipModuleInsertion: false,
     paths : {
-		backbone : 'libraries/backbone/backbone',
+		backbone : 'libraries/backbone/backbone.min',// ['http://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min', 'libraries/backbone/backbone'],
+		jquery : 'libraries/jquery/jquery-2.0.0.min',//['http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min', 'libraries/jquery/jquery'],
+		underscore : 'libraries/underscore/underscore.min',//['http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min','libraries/underscore/underscore'],
+		handlebars : 'libraries/handlebars/handlebarshelpers',
+		handlebarshelpers :'libraries/handlebars/handlebars',// ['http://cdnjs.cloudflare.com/ajax/libs/handlebars.js/1.0.0-rc.3/handlebars.min','libraries/handlebars/handlebars'],
+		css : 'libraries/require/css',
+		normalize : 'libraries/require/normalize',
+		async : 'libraries/require/async',
+		text : 'libraries/require/text',
+		facade : 'core/facade',
+		envvariables : 'core/envvariables',
+		sandbox : 'core/sandbox',
+		locallayer : 'core/locallayer',
+		mediator : 'core/mediator',
+		debugmode : 'libraries/debugmode/debugmode',
+		errorlogger : 'libraries/errorlogger/errorlogger',
+		persistence : 'libraries/lawnchair/lawnchair',
+		fbgraphinitializer : 'components/fbgraph-initializer/fbgraph-initializer'
+		
+		
+		/*,backbone : 'libraries/backbone/backbone',
 		jquery : 'libraries/jquery/jquery',
 		underscore : 'libraries/underscore/underscore',
 		handlebars : 'libraries/handlebars/handlebarshelpers',
@@ -76,7 +96,11 @@
 		debugmode : 'libraries/debugmode/debugmode',
 		errorlogger : 'libraries/errorlogger/errorlogger',
 		persistence : 'libraries/lawnchair/lawnchair',
-		fbgraphinitializer : 'components/fbgraph-initializer/fbgraph-initializer'
+		fbgraphinitializer : 'components/fbgraph-initializer/fbgraph-initializer'*/
+
+			
+			
+			
 	},
 	shim : {
 		'backbone' : {
