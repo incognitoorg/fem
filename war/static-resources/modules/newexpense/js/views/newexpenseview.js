dcd18de2484b73aac46356b6f3695da09aac1dc0
vishwanatharondekar <vishwanatharondekar@gmail.com>
2013-04-28 23:33:34 +0530

    New expense screen. Carousel capabilities with swipe.

--- war/static-resources/modules/newexpense/js/views/newexpenseview.js
+++ war/static-resources/modules/newexpense/js/views/newexpenseview.js
@@ -3,6 +3,8 @@ define(function(require) {
 	var Sandbox = require('sandbox');
 	var SelectGroup = require('modules/selectgroup/selectgroup');
 	var CSS = require('css!./../../css/newexpense.css');
+	var JqueryTouch = require('libraries/jquery-mobile/jquery.mobile.touch.min');
+	
 	
 	var NewExpenseView = Sandbox.View.extend({
 		initialize : function(options) {
@@ -48,21 +50,49 @@ define(function(require) {
 			     $(el).css({'margin-left':$(el).width()*index});
 			});
 			
-			$(element).find('.next').click(function(){
+			var showNextPage = function(){
 				var pageIndex = $.makeArray($(element).children()).indexOf($(this).parents('.item')[0]);
 				pageIndex +=1;
 				$(element).children().each(function(index, el){
 				     $(el).animate({'margin-left':$(el).width()*index - pageIndex*$(el).width()});
 				});
-			});
+			};
+			$(element).find('.next').click(showNextPage);
 			
-			$(element).find('.previous').click(function(){
+			var showPreviousPage = function(){
 				var pageIndex = $.makeArray($(element).children()).indexOf($(this).parents('.item')[0]);
 				pageIndex -=1;
 				$(element).children().each(function(index, el){
 				     $(el).animate({'margin-left':$(el).width()*index - pageIndex*$(el).width()});
 				});
+			};
+			$(element).find('.previous').click(showPreviousPage);
+			
+			$(element).height(function(){
+				var totalHeight = 0;
+				$($(element).find('.item')[0]).children().each(function(index, el){
+					totalHeight += $(el).height();
+				});
+				return totalHeight;
+			}());
+			
+			
+			$(element).find('.item')
+			.swipeleft(function(){
+				var pageIndex = $.makeArray($(element).children()).indexOf(this);
+				pageIndex=pageIndex+1!=$(element).children().size()?pageIndex+1:pageIndex;
+				$(element).children().each(function(index, el){
+				     $(el).animate({'margin-left':$(el).width()*index - pageIndex*$(el).width()});
+				});
+			})
+			.swiperight(function(){
+				var pageIndex = $.makeArray($(element).children()).indexOf(this);
+				pageIndex =pageIndex!=0?pageIndex-1:pageIndex;
+				$(element).children().each(function(index, el){
+				     $(el).animate({'margin-left':$(el).width()*index - pageIndex*$(el).width()});
+				});
 			});
+			
 		}
 	});
 	
