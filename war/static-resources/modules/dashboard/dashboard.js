3c53a36ab737a51b69cde83d3bcc6492a695f057
vishwanatharondekar <vishwanatharondekar@gmail.com>
2013-05-27 19:07:05 +0530

    Dashboard main file.

---
+++ war/static-resources/modules/dashboard/dashboard.js
@@ -0,0 +1,13 @@
+define(function(require) {
+	var DashboardView = require('./js/views/dashboardview');
+
+	return {
+		getInstance : function() {
+			return {
+				initialize : function(options) {
+					this.view = new DashboardView(options);
+				}
+			};
+		}
+	};
+});
