8e3a81eadd6ed7907d420d5749d99d42bf8fcfe9
vishwanatharondekar <vishwanatharondekar@gmail.com>
2013-07-11 15:17:51 +0530

    Envvariables

--- war/static-resources/core/envvariables.js
+++ war/static-resources/core/envvariables.js
@@ -20,7 +20,16 @@ define(function(require) {
 			GOOGLE_API_SCOPE : 'https://www.googleapis.com/auth/plus.me https://www.google.com/m8/feeds',
 			//GOOGLE_API_SCOPE : 'https://www.googleapis.com/auth/plus.me https://www.google.com/m8/feeds',
 			//https://www.googleapis.com/auth/userinfo.profile
-			
 		};
+	} else if(mode==="qa"){
+		return {
+			API_URL : "",
+			FB_APP_ID : '610955545604534',
+			GOOGLE_CLIENT_ID : '675356629669.apps.googleusercontent.com',
+			GOOGLE_API_KEY : 'AIzaSyCxvFWYp8uk3RxCSEaVEo_FLYeqQVUelpg',
+			GOOGLE_API_SCOPE : 'https://www.googleapis.com/auth/plus.me https://www.google.com/m8/feeds',
+			//GOOGLE_API_SCOPE : 'https://www.googleapis.com/auth/plus.me https://www.google.com/m8/feeds',
+			//https://www.googleapis.com/auth/userinfo.profile
+		}
 	}
 });
